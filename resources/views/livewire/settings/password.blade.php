597c277928dc99338106ec7193408e9a8c1eabb1
ViktorsMuthe <developer.intern@gpomagno.com.mx>
2025-02-27 09:05:06 -0600

    Traducir textos de configuración de usuario al español en las vistas de Livewire

--- resources/views/livewire/settings/password.blade.php
+++ resources/views/livewire/settings/password.blade.php
@@ -40,12 +40,12 @@ new class extends Component {
 <section class="w-full">
     @include('partials.settings-heading')
 
-    <x-settings.layout heading="{{ __('Update password') }}" subheading="{{ __('Ensure your account is using a long, random password to stay secure') }}">
+    <x-settings.layout heading="{{ __('Actualizar contraseña') }}" subheading="{{ __('Asegúrate de que tu cuenta use una contraseña larga y aleatoria para mantenerla segura') }}">
         <form wire:submit="updatePassword" class="mt-6 space-y-6">
             <flux:input
                 wire:model="current_password"
                 id="update_password_current_passwordpassword"
-                label="{{ __('Current password') }}"
+                label="{{ __('Contraseña actual') }}"
                 type="password"
                 name="current_password"
                 required
@@ -54,7 +54,7 @@ new class extends Component {
             <flux:input
                 wire:model="password"
                 id="update_password_password"
-                label="{{ __('New password') }}"
+                label="{{ __('Nueva contraseña') }}"
                 type="password"
                 name="password"
                 required
@@ -63,7 +63,7 @@ new class extends Component {
             <flux:input
                 wire:model="password_confirmation"
                 id="update_password_password_confirmation"
-                label="{{ __('Confirm Password') }}"
+                label="{{ __('Confirmar contraseña') }}"
                 type="password"
                 name="password_confirmation"
                 required
@@ -72,11 +72,11 @@ new class extends Component {
 
             <div class="flex items-center gap-4">
                 <div class="flex items-center justify-end">
-                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Save') }}</flux:button>
+                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Guardar') }}</flux:button>
                 </div>
 
                 <x-action-message class="me-3" on="password-updated">
-                    {{ __('Saved.') }}
+                    {{ __('Guardado.') }}
                 </x-action-message>
             </div>
         </form>
